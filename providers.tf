d7b9d58c2aa7a1d7c02ed83d1a367ca4749ce250
devopsbrhub <devopsbrhub@gmail.com>
2025-01-30 13:00:26 -0300

    Criando Providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,4 @@
+# Definindo o providers
 terraform {
   required_providers {
     aws = {
@@ -7,10 +8,10 @@ terraform {
   }
 }
 
+# Configuração do providers AWS
 provider "aws" {
   region = var.region
 
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-
 }
